e2e4c9f9e822dd3552fd21360ffe9eca959802c9
Subhashis Suara <subhashis.suara999@gmail.com>
2019-05-27 00:35:00 +0530

    Major tweaks for performance and exceptions

--- OneWordStoryBot.py
+++ OneWordStoryBot.py
@@ -43,7 +43,7 @@ def makestory():
     post_ids = []
     keywordpost_ids = []
     counter = 0
-    template = '6751df8a-7973-11e9-aa61-0e650c368d90' # Basically theme ID of flair
+    template = '6751df8a-7973-11e9-aa61-0e650c368d90'
 
     print("Collecting Posts from " + subreddit_name + "...\n")
     for i in range(len(result)):
@@ -54,48 +54,63 @@ def makestory():
     print("Searching for completed stories...\n")
     for j in range(post_ids_size): # Gathers id of all posts with 'the end' in comments made by author of the post
         submission = reddit.submission(post_ids[j])
-        submission.comments.replace_more(limit=None)
+        submission.comments.replace_more(limit = None)
         author = result[j].author
+
+        title = submission.title
+        title_split = title.split()
+
+        if len(title_split) == 1:
+            submission.flair.select(template, 'Story')
+
         for comment in submission.comments.list(): # Goes through comments of post; Note: .list() is used to flatten all hierarchy of comments into a single list
             comment_lower = comment.body.lower()
             keyword = re.search("the end", comment_lower) # Searches for 'the end' in the comments
             if keyword and comment.author == author:
-                keywordpost_ids.append(post_ids[j]) # Appends to a different list to sort out the posts with keywords from all posts
-            
-            title = submission.title
-            title_split = title.split()
-
-            if len(title_split) == 1: # As per the rules of the subreddit, a story post's title should be of one word
-                submission.flair.select(template, 'Story')
+                print("Completed story found! OP: u/" + author)
+                keywordpost_ids.append(post_ids[j])
+                break
+    print("\n")
 
     for k in range(len(keywordpost_ids)):
         words = []
         story = []
-        submission = reddit.submission(keywordpost_ids[k]) # Pulls the post using the post ID
+        submission = reddit.submission(keywordpost_ids[k])
         
-        if submission.saved: # To prevent going through read posts again
+        if submission.saved:
             continue
 
         title = submission.title
         words.append(title)
         title_split = title.split()
 
-        if len(title_split) == 1: # As per the rules of the subreddit, a story post's title should be of one word
-            for comment in submission.comments.list(): # Goes through comments of post; Note: .list() is used to flatten all hierarchy of comments into a single list
-                words.append(comment.body)
-                parent = comment
+        if len(title_split) == 1:
+            submissions = submission.comments.list()
+            while True:
+                words.append(submissions[0].body)
+                parent = submissions[0]
                 parent.reply_sort = 'old'
                 parent.refresh()
-                for reply in parent.replies.list(): # Collects the words for the story
+                parent.replies.replace_more(limit=None, threshold = 0)
+                for reply in parent.replies.list():
                     words.append(reply.body)
                 break
+
         for l in range(len(words)):
-            if words[l].lower() == "the end":
-                words[l] = words[l].lower()
-                words.remove("the end") # To remove the words 'the end' from the story as the only case possible is when the author tries to comment the keywords
-        story = ' '.join(words)
-        story_with_fullstop = (story + '.')
-        print("Baking and publishing a story by " + str(submission.author) + "!")
+            word_lower = words[l].lower()
+            if word_lower == "the end" or word_lower == "[deleted]" or word_lower == "[removed]":
+                words.remove(words[l]) # To remove the unnecessary words 
+
+        last_char = words[len(words) - 1]
+
+        if last_char == '''"''' or last_char == "!" or last_char == "?" or last_char == '''."''':
+            story = ' '.join(words)
+            story_with_fullstop = story 
+        else:
+            story = ' '.join(words)
+            story_with_fullstop = (story + '.')
+
+        print("Baking and publishing a story by u/" + str(submission.author) + "!")
         bot_reply = submission.reply(story_with_fullstop)
         bot_reply.mod.distinguish(sticky=True)
         submission.save()
@@ -114,8 +129,7 @@ def makestory():
 
 def main():
     makestory()
-
+   
 
 if __name__ == "__main__": 
     main()
-    
